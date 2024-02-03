a3abea57ee0fafd8f1e1b4a29a7b6ae5cc8bf70a
VINICIOS MATHEUS OLIVEIRA DA SILVA <106559003+viniciostec22@users.noreply.github.com>
2024-02-03 18:02:41 -0300

    feat: add initial search component

---
+++ app/(home)/_components/search.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Button } from "@/app/components/ui/button";
+import { Input } from "@/app/components/ui/input";
+import { SearchIcon } from "lucide-react";
+
+
+
+const Search = () => {
+    return (
+        <div className="flex items-center gap-2">
+          <Input placeholder="Busque por uma Barbearia..."/>
+          <Button variant="default" >
+            <SearchIcon size={20} />
+          </Button>
+
+        </div>
+    );
+};
+
+export default Search;
